b16d7fcffcf88c3a03e5fbbd5f780ff01a40388b
Dennis Lustre <dennis.lustre@gmail.com>
2023-10-05 15:22:48 -0700

    Add auto scroll to user ranking

--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,15 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
 import { fetchAllUsers } from './firebaseUtils'; // Adjust the import path
 
 export default function Leaderboard({ username }) {
     const [leaderboardData, setLeaderboardData] = useState([]);
+    const userRankingRef = useRef(null);
 
     // Fetch leaderboard data from Firebase and set it in state
-    const fetchData = async () => {
+    async function fetchData() {
         const data = await fetchAllUsers();
         setLeaderboardData(data);
-    };
+        scrollToUser();
+    }
+
+    function scrollToUser() {
+        if (userRankingRef.current) {
+            userRankingRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     useEffect(() => {
         fetchData();
@@ -28,6 +36,7 @@ export default function Leaderboard({ username }) {
                 {sortedLeaderboard.map((user, index) => (
                     <div
                         key={index}
+                        ref={user.username === username ? userRankingRef : null}
                         className={`leaderboardItem ${index === 0 ? 'first' : index === 1 ? 'second' : index === 2 ? 'third' : ''} ${user.username === username ? 'userRanking' : ''}`}
                     >
                         <span className="rank">{index + 1}. </span>
